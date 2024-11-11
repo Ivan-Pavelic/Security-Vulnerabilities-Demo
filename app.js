7182d04c96ae353a436a1e54824d78d76d1f131d
Ivan-Pavelic <ip54375@fer.hr>
2024-11-11 03:19:15 +0100

    Render redeploy #10

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 let redisClient = createClient({ url: process.env.REDIS_URL });
 redisClient.connect().catch(console.error);
 
